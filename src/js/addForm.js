604260655e5a243aa2e10fd4587dc4d354db5a2d
Fusted <tihantsov@gmail.com>
2021-11-15 00:27:03 +0300

    Form styled

--- src/js/addForm.js
+++ src/js/addForm.js
@@ -1,24 +1,47 @@
 import {addCard} from './card'
 
 function addForm(){
-    document.querySelector('.form').addEventListener('submit', (e) => {
+    const inputs = document.querySelectorAll('.important')
+    const form = document.querySelector('.form')
+    const btn = document.querySelector('.btn')
+
+
+    form.addEventListener('change', () => {
+        const notFilled = []
+        inputs.forEach(field => {
+            const input = field.querySelector('input')
+            if (!input.value){
+                notFilled.push(input)
+            } 
+        })
+        btn.classList.remove('btn-active')
+        
+        if (notFilled.length == 0){
+            btn.classList.add('btn-active')
+        }
+    })
+
+    form.addEventListener('submit', (e) => {
         e.preventDefault()
-        const inputs = document.querySelectorAll('.important')
         
         const filled = [],
               notFilled = []
-        inputs.forEach(input => {
+        inputs.forEach(field => {
+            const input = field.querySelector('input')
+
             if (input.value){
+                field.classList.remove('not-filled')
                 filled.push(input)
             } else {
+                field.classList.remove('filled')   
+                field.classList.add('not-filled')
                 notFilled.push(input)
             }
         })
 
-        
 
         if (notFilled.length > 0){
-            
+            btn.classList.remove('btn-active')
             notFilled.forEach(input => {
                 input.classList.remove('filled')
                 input.classList.add('not-filled')
@@ -27,11 +50,11 @@ function addForm(){
                 input.classList.remove('not-filled')
                 input.classList.add('filled')
             })
+
             console.log('not all forms filled')
 
         } else {
-            /* img, title, desc, price, id */
-
+        
             const newCard = {
                 img: document.querySelector('[data-form=img]').value,
                 title: document.querySelector('[data-form=title]').value,
@@ -39,7 +62,8 @@ function addForm(){
                 price: document.querySelector('[data-form=price]').value,
                 id: `${Math.floor(Math.random() * (1000000 - 100000)) + 10000}${document.querySelector('[data-form=title]').value}`,
             }
-            
+            form.reset()
+            document.querySelectorAll('.filled').forEach(item => item.classList.remove('filled'))
             addCard(newCard)
         }
     })
